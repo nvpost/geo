47cafd538a140907a7f3d19cf7504914fa305b6e
nik <nvbpost@yandex.ru>
2016-10-11 17:37:35 +0300

    11.10

--- js.js
+++ js.js
@@ -2,21 +2,30 @@ function cl(e){
 	console.log(e)
 }
 var qweNumber=parseInt(Math.random()*(geoBase.length-1));
-var currentQwe=geoBase[qweNumber][1]
-var currentAnsw=geoBase[qweNumber][2]
+
 var currentfalseAnsw=[]
-for(var i=2 ; i<geoBase[qweNumber].length; i++){
+for(var i=3 ; i<geoBase[qweNumber].length; i++){
 	if(geoBase[qweNumber][i]!==""){
 		currentfalseAnsw.push(geoBase[qweNumber][i])
 	}
 	
 }
-cl(currentQwe)
+indexFalse=0;
 function logic(){
 	var qweLogic=parseInt(Math.random()*2)
 	cl(qweLogic)
+	if(qweLogic==0){
+		$('.answer').find('span').html(geoBase[qweNumber][2])
+	}else{
+		cl('Вариант с неправильным')
+		var indexFalse=parseInt(Math.random()*currentfalseAnsw.length-1)
+		cl(currentfalseAnsw.length)
+		cl(indexFalse)
+		cl(currentfalseAnsw[indexFalse])
+		$('.answer').find('span').html(currentfalseAnsw[indexFalse])
+	}
 }
 logic()
 $('.theme').find('span').html(geoBase[qweNumber][0])
 $('.qweText').find('span').html(geoBase[qweNumber][1])
-$('.answer').find('span').html(geoBase[qweNumber][2])
+
